b27a268d2859dbf4708a297ef55c888dd77c5a79
KMcKraken <69874029+KMcKraken@users.noreply.github.com>
2023-09-08 04:34:01 -0500

    Shoelace

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,7 @@
 <script>
-
-    import SvelteMarkdown from 'svelte-markdown';
-
-    import EditText from "$comp/EditText.svelte";
-
+    
     import { onMount } from 'svelte';
+    /*
     import PocketBase from 'pocketbase';
 
     const pb = new PocketBase('http://127.0.0.1:8090');
@@ -16,43 +13,36 @@
         console.log(data);
         items = data;
     });
+    */
+
+    
+    onMount(() => {
+        {
+            const tooltip = document.querySelector('.manual-tooltip');
+            const toggle = document.querySelector('.manual-toggle');
+
+            toggle.addEventListener('click', () => (tooltip.open = !tooltip.open));
+        }
+    }) 
+
 
 </script>
 
 <div class="container">
 
-{#each items as item}
-    <div class="card">
-        <EditText
-            text="{item.name}"
-            charId="{item.id}"
-            fieldId="name"
-        />
-        <p class="race">{item.race}</p>
-        <EditText
-            text="{item.desc}"
-            charId="{item.id}"
-            fieldId="desc"
-        />
-        <EditText
-            text="{item.asp_1}"
-            charId="{item.id}"
-            fieldId="asp_1"
-        />
-        <EditText
-            text="{item.asp_2}"
-            charId="{item.id}"
-            fieldId="asp_2"
-        />
-        <EditText
-            text="{item.asp_3}"
-            charId="{item.id}"
-            fieldId="asp_3"
-        />
-    </div>
-{/each}
+    <sl-tooltip
+        trigger="manual"
+        placement="bottom"
+        class="manual-tooltip"
+    >
+        <div class="content" slot="content">
+            Suck a fat one, yooooo
+        </div>
 
-</div>
+        <sl-button class="manual-toggle">Toggle Manually</sl-button>
+    </sl-tooltip>
+
+</div>  
 
 <style lang="scss">
 
@@ -61,22 +51,29 @@
     .container {
         width: 100%;
         height: 100%;
+        padding: 1rem;
         display: flex;
         gap: 1rem;
         padding: 1rem;
     }
 
-    .card {
-        padding: 1rem;
-        width: 100%;
-        max-width: 350px;
+    #text {
+        display: inline-block;
+        width: 30vh;
         height: fit-content;
-        display: flex;
-        flex-direction: column;
-        gap: .4rem;
-        border: 1px solid $bg-2;
-        border-radius: .4rem;
+    }
 
+    #text ~ sl-popup .box {
+        width: 100%;
+        height: fit-content;
+        background: var(--sl-color-primary-600);
+        border-radius: var(--sl-border-radius-medium);
+    }
+
+    .card-header [slot='header'] {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
     }
 
 </style>
